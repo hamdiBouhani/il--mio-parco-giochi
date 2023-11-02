77ac165b9e7b47f9be3da462b3f2c596a596afdc
bouhaniHamdi <bouhanihamdi@gmail.com>
2023-11-02 12:41:23 +0100

    add new http server

--- README.md
+++ README.md
@@ -9,3 +9,14 @@ my playground projects test different technologies
 
 in the stack module we have the thread safe implementation of stack data structure using a list as the underlying data structure. Also, I have implemented a [Reverse Polish Notation](https://en.wikipedia.org/wiki/Reverse_Polish_notation) (RPN) using a stack data structure.
 
+
+
+---
+
+## hot reload:
+
+```bash
+go get github.com/cosmtrek/air
+go run github.com/cosmtrek/air
+```
+the visit the http://localhost:3000
--- main.go
+++ main.go
@@ -1,3 +1,74 @@
 package main
 
-func main() {}
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+)
+
+func init() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+}
+
+func indexHandler(c *fiber.Ctx, db *sql.DB) error {
+	return c.SendString("Hello")
+}
+
+func postHandler(c *fiber.Ctx, db *sql.DB) error {
+	return c.SendString("Hello")
+}
+
+func putHandler(c *fiber.Ctx, db *sql.DB) error {
+	return c.SendString("Hello")
+}
+
+func deleteHandler(c *fiber.Ctx, db *sql.DB) error {
+	return c.SendString("Hello")
+}
+
+func main() {
+
+	// Connect to database
+	connStr := fmt.Sprintf("postgresql://%s:%s@%s/%s?sslmode=disable",
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASS"),
+		os.Getenv("DB_URL"),
+		os.Getenv("DB_DATABASE"),
+	)
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return indexHandler(c, db)
+	})
+
+	app.Post("/", func(c *fiber.Ctx) error {
+		return postHandler(c, db)
+	})
+
+	app.Put("/update", func(c *fiber.Ctx) error {
+		return putHandler(c, db)
+	})
+
+	app.Delete("/delete", func(c *fiber.Ctx) error {
+		return deleteHandler(c, db)
+	})
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "3000"
+	}
+	log.Fatalln(app.Listen(fmt.Sprintf(":%v", port)))
+}
